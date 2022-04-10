e4de3475855b64b5840673c89ecbbcc24f48a155
emmefredericks <89538884+emmefredericks@users.noreply.github.com>
2022-04-10 17:04:41 -0600

    assignment 12

---
+++ js/assignment12.js
@@ -0,0 +1,26 @@
+const endpoint= 'http://www.boredapi.com/api/activity/';
+
+const activityButton = document.querySelector('#js-new-activity');
+activityButton.addEventListener('click',getActivity);
+
+async function getActivity() {
+  console.log("activity button was clicked");
+  try {
+    const response = await fetch(endpoint)
+    if (!response.ok){
+      throw Error(response.statusText)
+    }
+
+    const json = await response.json();
+    console.log(json);
+    displayActivity(json.activity);
+  } catch(err){
+    console.log(err)
+    alert('Failed');
+  }
+}
+
+function displayActivity(activity) {
+  const activityText= document.querySelector('#js-activity-text');
+  activityText.textContent = activity;
+}
